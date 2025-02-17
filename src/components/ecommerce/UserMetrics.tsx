c6da52bba1d5d7cfce7d9059aba7bd0a96033803
Logesh08 <logeshkrishna08@gmail.com>
2025-02-17 21:33:29 +0530

    Added login and admin previlages

--- src/components/ecommerce/UserMetrics.tsx
+++ src/components/ecommerce/UserMetrics.tsx
@@ -1,9 +1,49 @@
 "use client";
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import Badge from "../ui/badge/Badge";
 import { ArrowDownIcon, ArrowUpIcon, BoxIconLine, GroupIcon } from "@/icons";
+import { collection, getCountFromServer } from "firebase/firestore";
+import { firestore } from "@/lib/firebase";
 
 export const UserMetrics = () => {
+
+  const [contactsCount, setContactsCount] = useState<number | null>(null);
+  const [usersCount, setUsersCount] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchCounts = async () => {
+      try {
+        // Get count for "contacts" collection
+        const contactsRef = collection(firestore, "contacts");
+        const contactsSnapshot = await getCountFromServer(contactsRef);
+        const contactsTotal = contactsSnapshot.data().count;
+
+        // Get counts for "users" and "old_users" collections
+        const usersRef = collection(firestore, "users");
+        const oldUsersRef = collection(firestore, "old_users");
+
+        const [usersSnapshot, oldUsersSnapshot] = await Promise.all([
+          getCountFromServer(usersRef),
+          getCountFromServer(oldUsersRef),
+        ]);
+        const totalUsers = usersSnapshot.data().count + oldUsersSnapshot.data().count;
+
+        setContactsCount(contactsTotal);
+        setUsersCount(totalUsers);
+      } catch (err: any) {
+        console.error("Error fetching counts:", err);
+        setError("Failed to fetch metrics");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCounts();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
       {/* <!-- Metric Item Start --> */}
@@ -18,7 +58,7 @@ export const UserMetrics = () => {
               Users Signed Up
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              378
+              { (loading) ? "Loading..." : (error) ? "Error..." : usersCount }
             </h4>
           </div>
         </div>
@@ -33,10 +73,10 @@ export const UserMetrics = () => {
         <div className="flex items-end justify-between mt-5">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
-              Contact Us
+              Contacts Received
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
-              53
+            { (loading) ? "Loading..." : (error) ? "Error..." : contactsCount }
             </h4>
           </div>
         </div>
