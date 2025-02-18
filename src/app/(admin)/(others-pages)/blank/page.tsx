07feae8eca80dc9fc1c8154e6751ddc09cd8f2db
iashokk <ashokkumaravel02@gmail.com>
2025-02-18 08:56:50 +0530

    Ui changes

--- src/app/(admin)/(others-pages)/blank/page.tsx
+++ src/app/(admin)/(others-pages)/blank/page.tsx
@@ -6,6 +6,7 @@ import RecentOrders from "@/components/ecommerce/UsersSignedup";
 import DemographicCard from "@/components/ecommerce/DemographicCard";
 import RevenueDetails from "@/components/ecommerce/RevenueDetails";
 import DailyViews from "@/components/ecommerce/DailyViewsChart";
+import LogTable from "@/components/tables/LogTable";
 
 export const metadata: Metadata = {
   title: "NodSync Admin Dashboard",
@@ -30,16 +31,12 @@ export default function BlankPage() {
              
             </div>
           </div>
+         
       <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12">
-        <div className="mx-auto w-full max-w-[630px] text-center">
-          <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
-            Card Title Here
+      <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
+            Logs
           </h3>
-          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
-            Start putting content on grids or panels, you can also use different
-            combinations of grids.Please check out the dashboard and other pages
-          </p>
-        </div>
+      <LogTable/>
       </div>
     </div>
   );
---
+++ src/components/tables/LogTable.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+import Badge from "../ui/badge/Badge";
+import Image from "next/image";
+
+interface Order {
+  id: number;
+ 
+  time: string;
+  host: string;
+  messages: string;
+  status: string;
+}
+
+// Define the table data using the interface
+const tableData: Order[] = [
+  {
+    id: 1,
+    time: "Feb 17 , 2025",
+    host: "Local",
+    messages: "Feb 17 , 2025",
+    status: "Active", 
+  },
+  {
+    id: 2,
+    time: "Feb 17 , 2025",
+    host: "Local",
+    messages: "Feb 17 , 2025",
+    status: "Active", 
+  },
+  {
+    id: 3,
+    time: "Feb 17 , 2025",
+    host: "Local",
+    messages: "Feb 17 , 2025",
+    status: "Active", 
+  },
+  {
+    id: 1,
+    time: "Feb 17 , 2025",
+    host: "Local",
+    messages: "Feb 17 , 2025",
+    status: "Active", 
+  },
+  
+];
+
+export default function LogTable() {
+  return (
+    <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
+      <div className="max-w-full overflow-x-auto">
+        <div className="min-w-[1002px]">
+          <Table>
+            {/* Table Header */}
+            <TableHeader className="border-b border-gray-100 dark:border-white/[0.05]">
+              <TableRow>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Time
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Host
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                 Messages
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                >
+                  Status
+                </TableCell>
+              </TableRow>
+            </TableHeader>
+
+            {/* Table Body */}
+            <TableBody className="divide-y divide-gray-100 dark:divide-white/[0.05]">
+              {tableData.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell className="px-5 py-4 sm:px-6 text-start">
+                    <div className="flex items-center gap-3">
+                      {order.time}
+                      
+                    </div>
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    {order.host}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    {order.messages}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
+                    <Badge
+                      size="sm"
+                      color={
+                        order.status === "Active"
+                          ? "success"
+                          : order.status === "Pending"
+                          ? "warning"
+                          : "error"
+                      }
+                    >
+                      {order.status}
+                    </Badge>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+}
