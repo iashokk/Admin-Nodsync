1a69e2f71c82d09fd39887bd21f2da84b700e476
Logesh08 <logeshkrishna08@gmail.com>
2025-02-27 10:26:22 +0530

    Fixed user list in dashboard

--- src/components/ecommerce/UsersSignedup.tsx
+++ src/components/ecommerce/UsersSignedup.tsx
@@ -120,7 +120,7 @@ export default function UsersSignedup() {
   return (
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
       {/* Header */}
-      <div className="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
+      <div className="flex items-center justify-between mb-4 gap-2">
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
             Signed Up Users
